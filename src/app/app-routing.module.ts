994ba5a0555d4ba73c7736c93e738f926df01ef1
Frederick Gonzalez <Frederick Gonzalez>
2021-07-08 13:12:34 -0400

    commit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// Importo las rutas
+import { PruebaComponent } from './prueba/prueba.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'prueba', component: PruebaComponent //nueva ruta sin lazy loading
+  },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+  { path: 'courses', loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
